4d7a6a546993d1cf8ce4e31d8d10b948908410a3
Kiệt Lê <kietle06022004@gmail.com>
2026-05-16 11:44:14 +0700

    feat(auditlog): hoàn thiện chức năng auditlog

---
+++ MediCare.API/Interfaces/IAuditLogService.cs
@@ -0,0 +1,11 @@
+﻿using MediCare.API.DTOs;
+using static MediCare.API.DTOs.UserDTO;
+
+namespace MediCare.API.Interfaces
+{
+    public interface IAuditLogService
+    {
+        Task<PagedResponse<AuditLogResponse>> GetAllAsync(AuditLogQueryParams query);
+        Task<AuditLogResponse> GetByIdAsync(long id);
+    }
+}
